7998569fdb01e3ab2979abc8eb921dbfd86e871f
Yue Qi Fuh <qifuhyue@gmail.com>
2025-05-01 21:12:21 +0800

    #header update

--- newPHP/app/_head.php
+++ newPHP/app/_head.php
@@ -16,15 +16,15 @@ require_once __DIR__ . '/_base.php';
         <a href="/newPHP/app/index.php" class="header-logo">Beauty & Wellness</a>
             <nav class="header-nav">
                 <ul class="nav-list">
-                <li><a href="/newPHP/app/index.php" class="nav-link">Home</a></li>
-                <li><a href="/newPHP/app/product/all_product.php" class="nav-link">Products</a></li>
-                <li><a href="/newPHP/app/order/cart.php" class="nav-link">Cart</a></li>
+                <li><a href="/../index.php" class="nav-link">Home</a></li>
+                <li><a href="/../product/all_product.php" class="nav-link">Products</a></li>
+                <li><a href="/../order/cart.php" class="nav-link">Cart</a></li>
 
                     <?php if (isLoggedIn()): ?>
-                        <li><a href="/newPHP/app/auth/logout.php" class="nav-link">Logout</a></li>
+                        <li><a href="/../auth/logout.php" class="nav-link">Logout</a></li>
 
                     <?php else: ?>
-                        <li><a href="/newPHP/app/auth/login.php" class="nav-link">Log In</a></li>
+                        <li><a href="/../auth/login.php" class="nav-link">Log In</a></li>
 
                     <?php endif; ?>
                 </ul>
